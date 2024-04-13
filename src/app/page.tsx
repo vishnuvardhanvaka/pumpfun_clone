e135c4e5e4db72428ce8de2714f46e02812c4da7
vishnu <vishnu.vaka@solomonsint.com>
2024-04-13 10:40:48 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+
+
+export default function main() {
+  return (
+    <div className="text-white">
+      <nav>
+        <div className="m-2 flex items-center">
+          <img alt="Pump" className="mr-4 w-6 h-6" src="https://pump.fun/_next/image?url=%2Flogo.png&w=32&q=75" />
+          <div className=" grid grid-cols-2 text-sm text-center">
+            <h1>[twitter]</h1>
+            <h1 className="">[support]</h1>
+            <h1>[telegram]</h1>
+            <h1>[how it works]</h1>
+
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+
+}
